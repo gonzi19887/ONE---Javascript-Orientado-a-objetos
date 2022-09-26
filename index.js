1786cf5699d2ffdde74dc55a457066293f42a95a
Programación One <oscarmarley1988@gmail.com>
2022-09-26 22:27:38 +0000

    Finalizndo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //importar modulos clase
-import{Cliente}from './Cliente.js';
+import { Cliente } from './Cliente.js';
 import { cuentaCorriente } from './CuentaCorriente.js';
 
 //Aca empieza el codigo visualizador
@@ -23,20 +23,22 @@ saldoLeonardo = cuentaDeLeonardo.depositoEnCuenta(500);
 console.log(`El saldo actual (CuentaLeonardo) ` + saldoLeonardo);
 
 const cliente2 = new Cliente();
-cliente.nombreCliente = "luisa";
-cliente.dniCliente = "123457";
-cliente.rutCliente = "87654322";
+cliente2.nombreCliente = "luisa";
+cliente2.dniCliente = "123457";
+cliente2.rutCliente = "87654322";
 
 const cuentaDeLuisa = new cuentaCorriente();
 cuentaDeLuisa.numero = "2";
 cuentaDeLuisa.agencia = "002";
-cuentaDeLuisa.cliente = cliente;
+cuentaDeLuisa.cliente = cliente2;
 
-cuentaDeLeonardo.transferirParaCuenta(100,cuentaDeLuisa);
+cuentaDeLeonardo.transferirParaCuenta(100, cuentaDeLuisa);
+
+console.log("Cuenta de Luisa ", cuentaDeLuisa);
 
 
 const saldoLuisa = cuentaDeLuisa.verSaldo();
-console.log('El saldo actual (CuentaLuisa) ' +  saldoLuisa);
+console.log('El saldo actual (CuentaLuisa) ' + saldoLuisa);
 
 const saldoALeonardo = cuentaDeLeonardo.verSaldo();
 console.log(`El saldo actual (CuentaLeonardo) ` + saldoALeonardo);
