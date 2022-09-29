c95e9eda4348f8af7fefdaad5eaf500c180231a1
Programación One <oscarmarley1988@gmail.com>
2022-09-29 21:43:17 +0000

    Tipos

--- SistemaAutenticacion.js
+++ SistemaAutenticacion.js
@@ -1,6 +1,6 @@
 export class SistemaAutenticacion {
 	static login(usuario, clave) {
-		if ("autenticable" in usuario) {
+		if ("autenticable" in usuario && usuario.autenticable instanceof function) {
 			return usuario.autenticable(clave);
 		}else{
 			return false;
